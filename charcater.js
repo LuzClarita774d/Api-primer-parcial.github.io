ed20e72a6456248e52f8e63aa512aa9e724eb227
Luzclarita18 <145073306+Luzclarita18@users.noreply.github.com>
2024-06-12 18:32:27 -0500

    Termino
    Se agrego toda la parte final ya con los vinculos

--- charcater.js
+++ charcater.js
@@ -12,10 +12,11 @@ fetch(urlAPI + "/character/" + id)
                 <div class="detail">
                     <h1> ${character.name} </h1>
                     <h2>${character.status} - ${character.species}</h2>
+                    <a href="location.html?id=${character.id}">
                     <p>Last known location:</p>
                     <p>${character.origin.name}</p>
                     <p>First seen in:</p>
-                    <p>${character.location.name}</p>
+                    <p class="ama">${character.location.name}</p> </a>
                 </div>
                 </div>`
             characterContainer.innerHTML += characterTemplate
